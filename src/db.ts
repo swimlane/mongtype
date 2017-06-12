b63eda0a69b2e74e3165f11a8ca640d4c4518a1e
Austin McDaniel <amcdaniel2@gmail.com>
2017-06-12 12:48:25 -0500

    feat(retry): add auto-retry

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,6 @@
 import { UpdateWriteOpResult, ObjectID, MongoClient, Db, Collection } from 'mongodb';
 import { Injectable } from 'injection-js';
+import * as retry from 'retry';
 
 @Injectable()
 export class Database {
@@ -9,12 +10,21 @@ export class Database {
   
   get connection(): Promise<Db> {
     if (!this.db) {
-      try {
-        this.db = MongoClient.connect(this.uri);
-        console.log(`Mongo: Connection opened: ${this.uri}`);
-      } catch(e) {
-        console.error('Mongo: Connection error', e);
-      }
+      const operation = retry.operation();
+
+      operation.attempt((attempt) => {
+        this.db = new Promise(async (resolve, reject) => {
+          try {
+            const db = await MongoClient.connect(this.uri);
+            console.log(`Mongo: Connection opened: ${this.uri}`);
+            resolve(db);
+          } catch(e) {
+            if (operation.retry(e)) return;
+            console.error('Mongo: Connection error', e);
+            reject(e);
+          }
+        });
+      });
     }
 
     return this.db;
