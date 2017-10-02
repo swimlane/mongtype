e69ca9135ad69ea40bff0b98041dfe5becc6dd1e
Shaun Burdick <shaun.burdick@swimlane.com>
2017-10-02 11:05:38 -0400

    chore(): Remove 'dist/' chore(): add .gitignore chore(): limit npm to just 'dist/' folder

--- dist/decorators.d.ts
+++
@@ -1,4 +0,0 @@
-import { CollectionProps } from './types';
-export declare function Collection(props: CollectionProps): (target: any) => void;
-export declare function Before(...events: string[]): (target: any, name: string, descriptor: TypedPropertyDescriptor<any>) => void;
-export declare function After(...events: string[]): (target: any, name: string, descriptor: TypedPropertyDescriptor<any>) => void;
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import 'reflect-metadata';
-export * from './repository';
-export * from './decorators';
-export * from './db';
-export * from './types';
